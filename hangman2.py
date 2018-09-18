a09a1212a8df34a35bb518e7162feaf9d8a0480b
mrzcs <mrzcs@163.com>
2018-09-18 23:29:09 +0800

    upload

--- hangman2.py
+++ hangman2.py
@@ -34,15 +34,40 @@ HANGMAN_PICS = ['''
   O   |
  /|\  |
  / \  |
+     ===''', '''
+  +---+
+ [O   |
+ /|\  |
+ / \  |
+     ===''', '''
+  +---+
+ [O]  |
+ /|\  |
+ / \  |
      ===''']
-words = 'ant baboon badger bat bear beaver camel cat clam cobra cougar coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk lion lizard llama mole monkey moose mouse mule newt otter owl panda parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake spider stork swan tiger toad trout turkey turtle weasel whale wolf wombat zebra'.split()
+#words = 'ant baboon badger bat bear beaver camel cat clam cobra cougar coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk lion lizard llama mole monkey moose mouse mule newt otter owl panda parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake spider stork swan tiger toad trout turkey turtle weasel whale wolf wombat zebra'.split()
+#words = 'red orange yellow green blue indigo violet white black brown'.split()
+words = {'Colors': 'red orange yellow green blue indigo violet white black brown'.split(),
+         'Shapes': 'square, triangle rectangle circle ellipse rhombus trapezoid chevron pentagon hexagon septagon octagon'.split(),
+         'Fruits': 'apple orange lemon lime pear watermelon grape grapefruit cherry banana cataloupe mango straw berry tomato'.split(),
+        'Animals':'bat bear beaver cat cougar crab deer dog donkey duck eagle fish frog goat leech lion lizard monkey moose mouse otter owl panda python rabbit rat shark sheep skunk squid tiger turkey turtle weaselwhale wolf wombat zebra'.split()
+         }
 
-def getRandomWord(wordList):
+def getRandomWord_old(wordList):
     '''
         returns a random string from the passed list of strings
     '''
     wordIndex = random.randint(0, len(wordList) - 1)
     return wordList[wordIndex]
+
+def getRandomWord(wordDict):
+    '''
+        returns a random string from the passed list of strings
+    '''
+    wordKey = random.choice(list(wordDict.keys()))
+    wordIndex = random.randint(0, len(wordDict[wordKey]) -1 )
+    return [wordDict[wordKey][wordIndex], wordKey]
+
     
 def displayBoard(missedLetters, correctLetters, secretWord):
     print(HANGMAN_PICS[len(missedLetters)])
@@ -82,12 +107,29 @@ def playAgain():
           
 print('H A N G M A N')
 
+difficulty = 'X'
+
+while difficulty not in ['E', 'M', 'H']:
+    print('Enter difficulty: E - Easy, M - Medium, H - Hard')
+    difficulty = input().upper()
+
+if difficulty == 'M':
+    del HANGMAN_PICS[8]
+    del HANGMAN_PICS[7]
+    
+if difficulty == 'H':
+    del HANGMAN_PICS[8]
+    del HANGMAN_PICS[7]
+    del HANGMAN_PICS[5]
+    del HANGMAN_PICS[3]
+    
 missedLetters = ''
 correctLetters = ''
-secretWord = getRandomWord(words)
+secretWord, secretSet = getRandomWord(words)
 gameIsDone = False
 
 while True:
+    print('The secret word is in the set: ' + secretSet)
     displayBoard(missedLetters, correctLetters, secretWord)
 
     guess = getGuess(missedLetters + correctLetters)
@@ -118,7 +160,7 @@ while True:
             missedLetters = ''
             correctLetters = ''
             gameIsDone = False
-            secretWord = getRandomWord(words)
+            secretWord, secretSet = getRandomWord(words)
         else:
             break
         
